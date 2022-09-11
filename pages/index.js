16e98a001522f3e278d63c050f64f739d906ac9c
adoool <adibjadidi74@gmail.com>
2022-09-11 14:01:39 +0430

    fixed a bug

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
 import Script from "next/script";
-import Date from "../components/date";
 import Layout, { siteTitle } from "../components";
 import { getSortedPostsData } from "../lib/posts";
 import utilStyles from "../styles/utils.module.css";
