e8698863510548e2b9f7c05b5b81f6ba8a23f6f4
adoool <adibjadidi74@gmail.com>
2022-09-11 14:07:43 +0430

    fixed a bug

--- components/Date.js
+++ components/Date.js
@@ -1,8 +1,6 @@
 import { parseISO, format } from "date-fns";
 
 export default function Date({ dateString }) {
-  console.log("dateString", dateString);
-
   const date = parseISO(dateString);
   console.log("date", date);
   return <time dateTime={dateString}>{format(date, "LLLL d, yyyy")}</time>;
